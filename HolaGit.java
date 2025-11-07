f799dbe3cbd38878b8c10793d75394b429690eb1
didacsantolaria-code <didacsantolaria@alumnes.ilerna.com>
2025-11-07 19:00:19 +0100

    Aqui he puesto mi parte del archivo Java

--- HolaGit.java
+++ HolaGit.java
@@ -1,5 +1,6 @@
 public class HolaGit {
     public static void main(String[] args) {
         System.out.println("¡Hola Git! Proyecto colaborativo iniciado.");
+        System.out.println("Saludos desde Colaborador 1 - Desarrollador Frontend");
     }
 }
