4e2c7c1184a19c1bc55540d6ccac5063bfbf39c0
unknown <cescbelletboneu@alumnes.ilerna.com>
2025-11-07 19:16:08 +0100

    he cambiado el mensaje para mi presentación como segundo colaborador

--- HolaGit.java
+++ HolaGit.java
@@ -1,5 +1,1 @@
-public class HolaGit {
-    public static void main(String[] args) {
-        System.out.println("¡Hola Git! Proyecto colaborativo iniciado.");
-    }
-}
+public class HolaGit { public static void main(String[] args) { System.out.println("¡Hola Git! Proyecto colaborativo iniciado."); System.out.println("Saludos desde Colaborador 2 - Desarrollador Backend"); } }
